9f2872588422801c3a68c111ebd622f5044ba8aa
Enzo Villanueva <mp15265@gmail.com>
2023-07-12 21:25:32 -0300

    Más cambios en los apartados y en su scroll

--- js/config_page.js
+++ js/config_page.js
@@ -2,6 +2,40 @@ const apiKey = "a3aa373a1b65bb41fd52cf0fab69bb46";
 const urlMovie = "https://api.themoviedb.org/3/movie/";
 const urlImage = "https://image.tmdb.org/t/p/w500/";
 
+const arrowIcons_1 = document.querySelectorAll(".icon_1 ion-icon"),
+arrowIcons_2 = document.querySelectorAll(".icon_2 ion-icon"),
+seccionContent = document.querySelectorAll(".popular");
+
+
+const firstIcon = () => {
+    let scroll= Math.round(seccionContent[0].scrollLeft);
+    let scrollMax= seccionContent[0].scrollWidth - seccionContent[0].clientWidth;
+    arrowIcons_1[0].parentElement.style.display = scroll > 0 ? "flex" : "none";
+    arrowIcons_1[1].parentElement.style.display = scrollMax > scroll ? "flex" : "none";
+};
+
+const secondIcon = () => {
+    let scroll= Math.round(seccionContent[1].scrollLeft);
+    let scrollMax= seccionContent[1].scrollWidth - seccionContent[1].clientWidth;
+    arrowIcons_2[0].parentElement.style.display = scroll > 0 ? "flex" : "none";
+    arrowIcons_2[1].parentElement.style.display = scrollMax > scroll ? "flex" : "none";
+}
+
+arrowIcons_1.forEach(icon => {
+    icon.addEventListener("click", () => {
+        seccionContent[0].scrollLeft += icon.id === "left" ? -1000 : 1000;
+        setTimeout(() => firstIcon(), 50)
+    });
+});
+
+arrowIcons_2.forEach(icon => {
+    icon.addEventListener("click", () => {
+        seccionContent[1].scrollLeft += icon.id === "left" ? -1000 : 1000;
+        setTimeout(() => secondIcon(), 50)
+    });
+});
+
+// Apartado de peliculas.
 const popularFilms = async () => {
     const response = await fetch(`${urlMovie}popular?api_key=${apiKey}&language=es-MX`);
     if (response.status == 200) {
@@ -14,6 +48,7 @@ const popularFilms = async () => {
     }
 }
 
+// Apartado de series.
 const series = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}&language=es-MX`)
     if (response.status == 200){
@@ -32,10 +67,10 @@ function addFilmsHTML(data){
     let box = '';
     data.results.forEach(element => {
         box += `
-            <div class="film">
+            <li class="film">
                 <img class="poster" src="${urlImage}${element.poster_path}"></img>
                 <div class="titulo">${element.title}</div>
-            </div>`
+            </li>`
     });
     document.getElementById("popular").innerHTML = box;
 }
@@ -44,10 +79,10 @@ function addSeriesHTML(data){
     let box = '';
     data.results.forEach(element => {
         box += `
-            <div class="film">
+            <li class="film">
                 <img class=poster src="${urlImage}${element.poster_path}"></img>
                 <div class="titulo">${element.name}</div>
-            </div>`
+            </li>`
     });
     document.getElementById("series").innerHTML = box;
 }
