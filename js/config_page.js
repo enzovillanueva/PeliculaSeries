0dd3069857cd6e57b03e12e8d300d89ee1006249
Enzo Villanueva <mp15265@gmail.com>
2023-07-18 21:41:56 -0300

    Cambios en la pág. 'vista' para las series

--- js/config_page.js
+++ js/config_page.js
@@ -6,32 +6,18 @@ const arrowIcons_1 = document.querySelectorAll(".icon_1 ion-icon"),
 arrowIcons_2 = document.querySelectorAll(".icon_2 ion-icon"),
 seccionContent = document.querySelectorAll(".popular");
 
-
-const firstIcon = () => {
-    let scroll= Math.round(seccionContent[0].scrollLeft);
-    let scrollMax= seccionContent[0].scrollWidth - seccionContent[0].clientWidth;
-    arrowIcons_1[0].parentElement.style.display = scroll > 0 ? "flex" : "none";
-    arrowIcons_1[1].parentElement.style.display = scrollMax > scroll ? "flex" : "none";
-};
-
-const secondIcon = () => {
-    let scroll= Math.round(seccionContent[1].scrollLeft);
-    let scrollMax= seccionContent[1].scrollWidth - seccionContent[1].clientWidth;
-    arrowIcons_2[0].parentElement.style.display = scroll > 0 ? "flex" : "none";
-    arrowIcons_2[1].parentElement.style.display = scrollMax > scroll ? "flex" : "none";
-}
-
+// Scroll Slice
 arrowIcons_1.forEach(icon => {
     icon.addEventListener("click", () => {
         seccionContent[0].scrollLeft += icon.id === "left" ? -1000 : 1000;
-        setTimeout(() => firstIcon(), 50)
+        setTimeout(() => configIcon(0, arrowIcons_1), 50)
     });
 });
 
 arrowIcons_2.forEach(icon => {
     icon.addEventListener("click", () => {
         seccionContent[1].scrollLeft += icon.id === "left" ? -1000 : 1000;
-        setTimeout(() => secondIcon(), 50)
+        setTimeout(() => configIcon(1, arrowIcons_2), 50)
     });
 });
 
@@ -53,6 +39,7 @@ const series = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}&language=es-MX`)
     if (response.status == 200){
         const data = await response.json();
+        console.log(data)
         addSeriesHTML(data);
     }
 }
@@ -66,7 +53,7 @@ function addFilmsHTML(data){
     data.results.forEach(element => {
         box += `
             <!-- Si clickeo en algunas de estas peliculas, se enviará el ID de las mismas a la otra pág.-->
-            <a href="vista.html?${element.id}" class="film">
+            <a href="pages/vista.html?P${element.id}" class="film">
                 <img class="poster" src="${urlImage}${element.poster_path}"></img>
                 <div class="titulo">${element.title}</div>
             </a>`
@@ -78,10 +65,17 @@ function addSeriesHTML(data){
     let box = '';
     data.results.forEach(element => {
         box += `
-            <a href="vista.html" class="film">
+            <a href="pages/vista.html?S${element.id}" class="film">
                 <img class=poster src="${urlImage}${element.poster_path}"></img>
                 <div class="titulo">${element.name}</div>
             </a>`
     });
     document.getElementById("series").innerHTML = box;
+}
+
+function configIcon(number, arrow) {
+    let scroll= Math.round(seccionContent[number].scrollLeft);
+    let scrollMax= seccionContent[number].scrollWidth - seccionContent[number].clientWidth;
+    arrow[0].parentElement.style.display = scroll > 0 ? "flex" : "none";
+    arrow[1].parentElement.style.display = scrollMax > scroll ? "flex" : "none";
 }
--- js/config_vista.js
+++ js/config_vista.js
@@ -1,12 +1,13 @@
 
 const apiKey = "a3aa373a1b65bb41fd52cf0fab69bb46",
-url = "https://api.themoviedb.org/3/movie/",
+urlMovie = "https://api.themoviedb.org/3/movie/",
+urlTV = "https://api.themoviedb.org/3/tv/",
 urlImage = "https://image.tmdb.org/t/p/w500/";
 
 
 // Obtengo el ID de la pelicula o serie.
-const cons = location.search.substring(1);
-console.log(cons);
+const query = location.search.substring(1);
+const type = query[0], id = query.slice(1);
 
 const title = document.getElementById("titulo"),
 poster = document.getElementById("poster"),
@@ -20,12 +21,12 @@ value_cp = document.querySelector(".value-circle");
 let progreso = 0, progresoFinal = 60, velocidad = 10;
 
 const information = async () => {
-    const response = await fetch(`${url}${cons}?api_key=${apiKey}&language=es-MX`);
+    const response = await fetch(`${type == 'P'? urlMovie : urlTV }${id}?api_key=${apiKey}&language=es-MX`);
     if (response.status == 200){
         const data = await response.json();
         console.log(data)
-        addInformation(data);
-    };
+        addInformation(data, type);
+    }
 }
 
 const progress = setInterval(() => {
@@ -47,12 +48,13 @@ function addGenres(data) {
     document.getElementById("genres").innerHTML = box;
 }
 
-function addInformation(data) {
-    document.title = data.title;
-    title.innerHTML = `${data.title} (${data.release_date.slice(0,4)})`;
+function addInformation(data, type) {
+    document.title = (type == 'P') ? data.title : data.name;
+    date_type = (type == 'P') ?  data.release_date : data.first_air_date;
+    title.innerHTML = `${type == 'P' ? data.title : data.name} (${date_type.slice(0,4)})`;
     poster.src = `${urlImage}${data.poster_path}`;
     sinopsis.innerHTML = data.overview;
     progresoFinal = Math.round(data.vote_average * 10);
-    addGenres(data)
+    addGenres(data);
 }
 
